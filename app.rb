1f0beca78f0e7c1c6ca8ef8752af832b10a628a5
wikimo <gwikimo@gmail.com>
2014-05-12 10:13:36 +0800

    update notify file

--- app.rb
+++ app.rb
@@ -33,7 +33,7 @@ post '/notify' do
   url = params[:url]
 
   notify_file = File.join(File.dirname(__FILE__),'/public/notify.htm')
-  File.open(notify_file) do |f|
+  File.open(notify_file,'w') do |f|
     f.puts "code ==> #{code} | message ==> #{message} | url ==> #{url} "
   end
 
